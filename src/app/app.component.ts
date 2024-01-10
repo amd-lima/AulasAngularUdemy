4e4e089ce5cfcc0f5f90b64560183625d3727254
Amanda Lima <delimaluiz.amanda@gmail.com>
2024-01-10 19:24:18 -0300

    introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NewComponentComponent } from './components/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [CommonModule, RouterOutlet, NewComponentComponent],
+  template: `<router-outlet></router-outlet>
+  <h1>Curso Angular</h1>
+  <div class="theme-dark">
+    <app-new-component/>
+  </div>
+  <app-new-component/>
+  `,
 })
 export class AppComponent {
   title = 'aulas-angular17';
