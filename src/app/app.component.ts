9a4d5ea7a941b204402a0702e384eddb99d7728d
Amanda Lima <delimaluiz.amanda@gmail.com>
2024-01-13 23:45:15 -0300

    template variable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NewComponentComponent } from './components/new-component/new-component.component';
 import { TemplateBindingComponent } from './components/template/template-binding/template-binding.component';
+import { TemplateVariablesComponent } from './components/template/template-variables/template-variables.component';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,12 @@ import { TemplateBindingComponent } from './components/template/template-binding
     RouterOutlet,
     NewComponentComponent,
     TemplateBindingComponent,
+    TemplateVariablesComponent
   ],
   template: `<router-outlet></router-outlet>
     <h1>Curso Angular</h1>
-    <app-template-binding /> `,
+    <app-template-binding />
+    <app-template-variables />`,
 })
 export class AppComponent {
   title = 'aulas-angular17';
