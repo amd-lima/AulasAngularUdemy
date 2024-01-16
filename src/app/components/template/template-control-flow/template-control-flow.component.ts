e2144cb0a73e92e7058e98208a2ac16cb605fc1f
Amanda Lima <delimaluiz.amanda@gmail.com>
2024-01-15 21:24:26 -0300

    control flow

--- src/app/components/template/template-control-flow/template-control-flow.component.ts
+++ src/app/components/template/template-control-flow/template-control-flow.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Observable, delay, of } from 'rxjs';
 
 @Component({
   selector: 'app-template-control-flow',
@@ -10,5 +11,23 @@ import { Component } from '@angular/core';
 })
 export class TemplateControlFlowComponent {
   public isTrue = true;
+
+  public loadingData$: Observable<String[]> = of([
+    'data1', 'data2', 'data3'
+  ]).pipe(delay(2000));
+
+  public trackByFn(index: number){
+    return index;
+  }
+
+
+  public itens = [{name: 'Amanda Lima'}]
+  public itensEmpty: Array<{name: string}> = []
+  public switchCondition: string = 'A'
+
+
+  addNewName(value: string){
+    this.itens.push({name:value});
+  }
 
 }
